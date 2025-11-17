fd4eea75d23115cde0ce4f398a66a4faf7554211
sahasy7 <sahasy7@gmail.com>
2025-11-17 13:00:52 +0530

    changes in server.js

--- src/server.js
+++ src/server.js
@@ -13,10 +13,7 @@ import dotenv from 'dotenv';
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config();
 
 const app = express();
 
